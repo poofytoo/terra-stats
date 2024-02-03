18e07cd5e72da5426c70f50ddafd6fa273f6d2e9
Victor Hung <victorhung92@gmail.com>
2024-02-03 12:13:10 -0500

    Show top and bottom

--- components/Corporations/Corporations.tsx
+++ components/Corporations/Corporations.tsx
@@ -50,25 +50,31 @@ export const Corporations = ({ data }: { data: Game[] }) => {
     return (b[1].wins ?? 0) * 10000 - (a[1].wins ?? 0) * 10000 + (a[1].plays ?? 0) - (b[1].plays ?? 0);
   });
 
+  const topCorporationCount = 6
+  const bottomCorporationCount = 6
+
   return <div className={styles.corporationDataContainer}>
     <h2>Wins by Corporation (all wins/plays) <span className={styles.highlight}>(main wins/plays)</span></h2>
     <p>
       All win/plays include corps which were mergered in. Main is only the initial corp.
     </p>
-    {sortedCorporations.filter((_, id) => showAll ? true : id < 8).map(([corporation, corporationStats]) => (
+    {sortedCorporations.filter((_, id) => showAll ? true : (id < topCorporationCount) || (id > sortedCorporations.length - bottomCorporationCount - 1)).map(([corporation, corporationStats], id) => (<>
+      {(id === topCorporationCount && !showAll) && <div key={'...'}>
+        <div>
+          ...
+        </div>
+      </div>}
       <div key={corporation}>
         <div>
           {corporation} ({corporationStats.wins ?? 0}/{corporationStats.plays}){" "}
           <span className={styles.highlight}>({corporationStats.mainWins ?? 0}/{corporationStats.mainPlays ?? 0})</span>
         </div>
-      </div>
+      </div></>
     ))}
-    {!showAll &&
-      <p>
-        <button onClick={() => {
-          setShowAll(!showAll);
-        }}>show all</button>
-      </p>
-    }
+    <p>
+      <button onClick={() => {
+        setShowAll(!showAll);
+      }}>{showAll ? "Hide" : "Show all"}</button>
+    </p>
   </div>
 }
